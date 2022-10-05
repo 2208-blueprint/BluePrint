1d4817de90fd73689e05b7f8b468e6d14aa727d2
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-10-05 22:26:09 +0100

    added social network login buttons and basic div structure of login form

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Google from './images/google.png'
+import Facebook from './images/facebook.png'
+import Github from './images/github.png'
 
 function LoginPage() {
 
@@ -7,10 +10,24 @@ function LoginPage() {
     <div className='login-main-container'>
         <div className='login-signup-wrapper'>
             <div className='login-signup-form-container'>
-
+              <div className='login-form-container-top'>
+                <h1>Login</h1>
+                <p>Login using social networks</p>
+                <div className='login-form-container-social-buttons'>
+                    <div className='loginButton google'>
+                        <img src={Google} alt='' className='icon' />
+                    </div>
+                    <div className='loginButton facebook'>
+                        <img src={Facebook} alt='' className='icon' />
+                    </div>
+                    <div className='loginButton github'>
+                        <img src={Github} alt='' className='icon' />
+                    </div>
+                    </div>
+              </div>
             </div>
             <div className='login-signup-toggle-container'>
-
+              test2
             </div>
         </div>
     </div>
