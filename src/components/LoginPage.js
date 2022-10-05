f0f033071d82f5f2b23ac5ade675dc5084772f8c
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-10-06 00:32:03 +0100

    Login form added with states and css

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Google from './images/google.png'
 import Facebook from './images/facebook.png'
 import Github from './images/github.png'
+import triangleBG from './images/trianglify-lowres.png'
 
 function LoginPage() {
+
+  const [username, setUserName] = React.useState('')
+  const [password, setPassword] = React.useState('')
 
 
   return (
@@ -11,8 +15,8 @@ function LoginPage() {
         <div className='login-signup-wrapper'>
             <div className='login-signup-form-container'>
               <div className='login-form-container-top'>
-                <h1>Login</h1>
-                <p>Login using social networks</p>
+                <h1>Login to your account</h1>
+                <small>Login using social networks</small>
                 <div className='login-form-container-social-buttons'>
                     <div className='loginButton google'>
                         <img src={Google} alt='' className='icon' />
@@ -29,9 +33,18 @@ function LoginPage() {
                     <div className='login-line'></div>
                     <div className='login-or'>OR</div>
                 </div>
-            </div>
+              <div className='login-form-bottom'>
+                <form className='login-form'>
+                    <input type='text' placeholder='Username' onChange={(e) => setUserName(e.target.value)} value={username}/>
+                    <input type='text' placeholder='Password' onChange={(e) => setPassword(e.target.value)} value={password}/>
+                    <button className='login-submit-button'>Login</button>
+                </form>
+                  </div>
+              </div>
             <div className='login-signup-toggle-container'>
-              test2
+              <h1>New to BluePrint?</h1>
+              <small>Sign up and start building!</small>
+              <button>Sign Up</button>
             </div>
         </div>
     </div>
