1fb0e8881817bbc54ca5b9a79275ad45c9bbbb12
Alec B <alec.butterfield1@gmail.com>
2022-10-06 12:41:38 -0700

    Main Page Skeleton first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { SingleComponent } from "./components";
 import { LoginPage } from "./components";
+import { MainPageSkeleton } from "./components"
 
 function App() {
 
   return (
     <>
     <Routes>
+      <Route path='/' element={<MainPageSkeleton/>}></Route>
       <Route path='/singlecomponent' element={<SingleComponent/>}></Route>
     </Routes>
     </>
---
+++ src/components/MainPage/MainPageSkeleton.js
@@ -0,0 +1,111 @@
+import React from "react";
+
+function MainPageSkeleton () {
+
+    return (
+        <div>
+            <div className="main-page-categories-container"> Categories
+                    Main Page Single Featured Component
+            </div>
+            <div className="main-page-outer-component-container">
+                <div className="main-page-single-featured-component">
+                    Main Page Featured Components
+                </div>
+                <div className="main-page-listed-component-container">
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                    <div className="main-page-single-component-container">
+                        Main Page Single Component Container
+                        <div className="main-page-single-component-title">
+                            Main Page Single Component Title
+                        </div>
+                        <div className="main-page-single-component-description">
+                            Main Page Single Component Description
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainPageSkeleton;
