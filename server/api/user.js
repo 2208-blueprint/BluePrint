8d77d8000027a3eb91786d5b629062fd38132767
Benjamin Lee <benjaminlee0727@gmail.com>
2022-10-06 16:21:00 -0500

    fixed req to res and commented code

--- server/api/user.js
+++ server/api/user.js
@@ -6,59 +6,66 @@ const requireToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     const user = await User.findByToken(token);
-    req.user = user;
+    res.user = user;
     next();
   } catch (error) {
     next(error);
   }
 };
-
+//sends components user has saved but is not author of
 router.get("/components", requireToken, async (req, res, next) => {
   try {
     const user = req.user;
     const components = await user.getComponents();
-    //filter out created
-    req.send(components);
+    const savedComponents = components.filter(
+      (item) => item.user_component.dataValues.isAuthor === false
+    );
+    res.send(components);
   } catch (error) {
     next(error);
   }
 });
+//sends list of creators user is following
 router.get("/creators", requireToken, async (req, res, next) => {
   try {
     const user = req.user;
     const creators = await user.getFollowing();
-    req.send(creators);
+    res.send(creators);
   } catch (error) {
     next(error);
   }
 });
+//sends list of followers following user
 router.get("/followers", requireToken, async (req, res, next) => {
   try {
     const user = req.user;
     const followers = await user.getFollowers();
-    req.send(followers);
+    res.send(followers);
   } catch (error) {
     next(error);
   }
 });
+
+//follow a user
 router.put("/follow/:userId", requireToken, async (req, res, next) => {
   try {
     const id = req.params.userId;
     const user = req.user;
     const creator = await User.findByPk(id);
     await user.addFollowing(creator);
-    req.send(user);
+    res.send(user);
   } catch (error) {
     next(error);
   }
 });
+//unfollow a user
 router.put("/unfollow/:userId", requireToken, async (req, res, next) => {
   try {
     const id = req.params.userId;
     const user = req.user;
     const creator = await User.findByPk(id);
     await user.removeFollowing(creator);
-    req.send(user);
+    res.send(user);
   } catch (error) {
     next(error);
   }
