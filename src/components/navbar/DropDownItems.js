d4798f6fdf51699688c5d603e96e8c331d6e11e5
Alec B <alec.butterfield1@gmail.com>
2022-10-07 13:42:04 -0700

    Footer added

---
+++ src/components/navbar/DropDownItems.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+function DropDownItems({ setDropDown, loggedIn, setLoggedIn }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    setLoggedIn(false);
+    // dispatch(toggle())
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="navbar-dropdown-container">
+        <li className="navbar-dropdown-item" onClick={() => setDropDown(false)}>
+          <Link to={window.localStorage.token ? "/profile" : "/login"}>
+            👤 Profile
+          </Link>
+        </li>
+        <li className="navbar-dropdown-item" onClick={() => setDropDown(false)}>
+          <Link to="/profile/checkout">🛒 Your Cart</Link>
+        </li>
+        <li className="navbar-dropdown-item" onClick={() => setDropDown(false)}>
+          {loggedIn ? (
+            <button onClick={() => logout()}>Logout</button>
+          ) : (
+            <button onClick={() => navigate("/login")}>Login</button>
+          )}
+        </li>
+      </div>
+    </>
+  );
+}
+
+export default DropDownItems
