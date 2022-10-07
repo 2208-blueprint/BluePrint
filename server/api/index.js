704e46dc46952da3bb2e86ca6fdc2c3133f2124d
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-10-07 01:20:22 +0100

    Passport js now working on all 3 social media logins, JWT almost fully implemented

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 // put your api routes here, for example:
 
 // router.use('/admin', require('./admin.js'))
-// router.use("/auth", require("./auth.js"));
+router.use("/auth", require("./auth.js"));
 
 module.exports = router;
