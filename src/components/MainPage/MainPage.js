0241c23aaf18e9519524265d5f1af2b64e9cd344
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-10-08 22:06:53 +0100

    Created mainpage div structure

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -2,116 +2,24 @@ import React from "react";
 
 function MainPage () {
 
+    const nums = ['1','2','3','4','5','6','7','8','9', '10', '11', '12']
+
     return (
         <div className="main-page-main-container">
-            <div className="main-page-categories-container"> 
-                Main Page Categories Container
-            </div>
-            <div className="main-page-outer-component-container">
-                Main Page Outer Component Container
-                <div className="main-page-single-featured-component">
-                    Main Page Featured Component
+            <div className="main-page-wrapper">
+                <div className="main-page-category-container">
+                Categories
                 </div>
-                <div className="main-page-listed-component-container">
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
+                <div className="main-page-content-container">
+                    <div className="main-page-featured-container">
+                        Featured component
                     </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
-                    </div>
-                    <div className="main-page-single-component-container">
-                        Main Page Single Component Container
-                        <img className="main-page-single-component-img"/>
-                        <div className="main-page-single-component-title">
-                            Main Page Single Component Title
-                        </div>
-                        <div className="main-page-single-component-description">
-                            Main Page Single Component Description
-                        </div>
+                    <div className="main-page-list-content-container">
+                        {nums.map((num, i) => {
+                            return <div key={i} className="main-page-content-list-element">
+                                {num}
+                            </div>
+                        })}
                     </div>
                 </div>
             </div>
