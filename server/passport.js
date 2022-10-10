2b4f4de2968d9175255f5ca8f8fda7fe2d6ec507
Benjamin Lee <benjaminlee0727@gmail.com>
2022-10-10 11:51:15 -0500

    deleted twitter from passportjs

--- server/passport.js
+++ server/passport.js
@@ -1,83 +1,80 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const GithubStrategy = require('passport-github2').Strategy;
-const TwitterStrategy = require('passport-twitter').Strategy;
-const TwitchStrategy = require('passport-twitch-new').Strategy;
-const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
-const passport = require('passport')
-const User = require('./db/User')
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const TwitchStrategy = require("passport-twitch-new").Strategy;
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./db/User");
 
 if (process.env.NODE_ENV !== "production") {
-    require("../secrets.js");
+  require("../secrets.js");
 }
 
-let opts = {}
+let opts = {};
 opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
+opts.secretOrKey = "secret";
 
-passport.use(new JWTStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
+passport.use(
+  new JWTStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
     });
-}));
+  })
+);
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    done(null, profile)
-  }
-));
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: ["user:email"],
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
 
-passport.use(new GithubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    scope: ['user:email'],
-    callbackURL: "/auth/github/callback",
-  },
-  function(accessToken, refreshToken, profile, done) {
-    done(null, profile)
-  }
-  ));
-
-passport.use(new TwitchStrategy({
-    clientID: process.env.TWITCH_CLIENT_ID,
-    clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    scope: ['user_read'],
-    callbackURL: "/auth/twitch/callback",
-  },
-  function(accessToken, refreshToken, profile, done) {
-    done(null, profile)
-  }
-));
-
-passport.use(new TwitterStrategy({
-    consumerKey: process.env.TWITTER_CLIENT_ID,
-    consumerSecret: process.env.TWITTER_CLIENT_SECRET,
-    callbackURL: "/auth/twitter/callback",
-    includeEmail: true
-  },
-  function(token, tokenSecret, profile, done) {
-
-    done(null, profile)
-  }
-));
+passport.use(
+  new TwitchStrategy(
+    {
+      clientID: process.env.TWITCH_CLIENT_ID,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET,
+      scope: ["user_read"],
+      callbackURL: "/auth/twitch/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user)
-})
+  done(null, user);
+});
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-  });
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
