f1e414b2accd14adc4d3e95a418f209ca8464661
Benjamin Lee <benjaminlee0727@gmail.com>
2022-10-10 11:57:30 -0500

    updated db file

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,22 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/blueprint",
-  { logging: false }
-);
 
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING === "true") {
+  delete config.logging;
+}
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/blueprint`,
+  config
+);
 module.exports = db;
