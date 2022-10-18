ebdc0e3f8a64f35afb5da3958c78a21dafb73529
Alec B <alec.butterfield1@gmail.com>
2022-10-18 07:09:41 -0700

    Search bar added to user page

--- src/components/users/UserPage.js
+++ src/components/users/UserPage.js
@@ -4,6 +4,9 @@ import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 import UserPageComponentCard from "./UserPageComponentCard";
 import ComponentCard from "../MainPage/ComponentCard";
+import axios from "axios";
+import { BsSearch } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 //This is the full "profile page" of a user. The logged-in user can visit this page to see components made by this
 //user, as well as follow the user to see more content from them.
@@ -12,6 +15,12 @@ function UserPage() {
 
     let navigate = useNavigate()
     const [curUser, setCurUser] = useState('')
+    const [rank, setRank] = useState("");
+    const [rankColor, setRankColor] = useState("");
+    const [points, setPoints] = useState(0)
+    const [totalFavs, setTotalFavs] = useState(0);
+    const [totalSaves, setTotalSaves] = useState(0);
+    const [searchBarInput, setSearchBarInput] = useState("");
     
     //Get current user number from URL path
     let curUserNum = window.location.pathname
@@ -19,35 +28,100 @@ function UserPage() {
 
     React.useEffect(() => {
 
-        async function getAllUsers() {
+        async function getCurrentUser() {
             try{
-                let curUser = await Axios.get(`/api/users/${curUserNum}`);
-                setCurUser(curUser.data) 
+                if (curUserNum) {
+            
+                const { data } = await Axios.get(`/api/users/${curUserNum}`);
+                setCurUser(data) 
+                console.log('First data: ',data)
+
+                if (data.highestRank >= 1000) {
+                    setRank("Chief");
+                    setRankColor("#FDFDBE");
+                  } else if (data.highestRank >= 500) {
+                    setRank("Engineer");
+                    setRankColor("#82EEE8");
+                  } else if (data.highestRank >= 200) {
+                    setRank("Architect");
+                    setRankColor("#FFD700");
+                  } else if (data.highestRank >= 100) {
+                    setRank("Artisan");
+                    setRankColor("#C0C0C0");
+                  } else {
+                    setRank("Apprentice");
+                    setRankColor("#CD7F32");
+                  }
+            
+                console.log('Data: ', data)
+                
+                }
             }
             catch(e) {
             console.error(e)
             }
         }
-        getAllUsers()
+        getCurrentUser() 
 
+        // if (!points){
+        //     setPoints(curUser.points)
+        // }
+        
     },[])
 
+    React.useEffect(() => {
+        async function getComponentSavesAndFavs() {
+            try {
+
+            // if (curUser) {
+            
+            const {data}  = await axios.get("/api/components")
+            console.log('Get Components Data: ', data);
+            console.log('First Component: ', data[0].users[0].username)
+            console.log('curUser.username: ', curUser)
+              const userComponents = data.filter(component => 
+                (component.users[0].username) === curUser?.username);
+                console.log('get Component Saves and Favs: ',userComponents)
+            //   }
+            } catch(e) {
+                console.error(e)
+            }
+
+            
+            //   parsedData.push({
+            //     id: user.id,
+            //     username: user.username,
+            //     componentCount,
+            //     followerCount,
+            //     points: user.currentPoints,
+            //   });
+            
+            // setTopUsers(parsedData);
+          };
+        getComponentSavesAndFavs()
+    },[])
+
+
+
+    console.log('Cur user: ', curUser)
+    
+    
+
     let componentsArray = curUser?.components
-
-    console.log('Cur User: ', curUser)
-
-    if (componentsArray){
-        console.log('Components Array: ', componentsArray[0])
-    }
   
-
-    function handleSelectComponent(evt){
-        evt.preventDefault()
-        navigate(`/components/${evt.target.id}`)
-    }
     function handleMessageMeButton(evt){
         evt.preventDefault()
     }
+
+    const handleSearch = () => {
+        if (searchBarInput === "") {
+          return;
+        }
+        let keywords = searchBarInput.split(" ").join("+");
+        setSearchBarInput("");
+        navigate(`/components/search/${keywords}`);
+      };
+
     return(
         <>
         <div className="single-user-page-main-container">
@@ -69,16 +143,27 @@ function UserPage() {
             </div>
                 <div className="single-user-page-user-stats-container">
                     <span>Components made: {curUser?.components?.length}</span>
-                    <span>Followers:</span>
+                    <span>Followers: {curUser?.followers?.length}</span>
                     <span>Total Favorites:</span>
                     <span>Total Saves:</span>
-                    <span>Bluepoints:</span>
+                    <span>Bluepoints: {points}</span>
                 </div>
             </div>
             <div className="single-user-page-search-container">
-                <div className="single-user-page-search-bar">
-                    Search bar
-                </div>
+                <input
+                    type="text"
+                    className="side-bar-container-searchbar"
+                    placeholder="SEARCH COMPONENTS"
+                    onChange={(e) => {
+                        setSearchBarInput(e.target.value);
+                    }}
+                    />
+                    <div className="side-bar-search-button" onClick={handleSearch}>
+                        {" "}
+                        <IconContext.Provider value={{ size: "20px" }}>
+                            <BsSearch />
+                        </IconContext.Provider>
+                    </div>
                 <div className="single-user-page-filter">
                     Filter by...
                 </div>
@@ -88,9 +173,7 @@ function UserPage() {
                     <div className="single-user-page-single-component">
                         <ComponentCard componentId={component.id} key={i} />
                     </div>
-                )
-                    // <UserPageComponentCard componentId = {component.id} key = {i}/>
-    
+                    )   
                 )}
             </div>           
         </div>
